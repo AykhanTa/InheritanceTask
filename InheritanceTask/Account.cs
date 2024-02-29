529b7a6d982a3546b2f3be96b6ab8c1139f470cb
AykhanTa <aykhanft@code.edu.az>
2024-02-29 18:49:15 +0400

    Add project files.

---
+++ InheritanceTask/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTask
+{
+    internal class Account
+    {
+        public string Username;
+        public string Password;
+        public Account()
+        {
+
+        }
+        public void Login(string username, string password)
+        {
+           if (username == "Admin" && password == "AdminEA18yr@")
+            {
+                Console.WriteLine("Giris ugurludur!");
+            }
+            else
+            {
+                Console.WriteLine("USername ve ya password duzgun daxil edilmeyib.");
+            }
+        }
+    }
+}
---
+++ InheritanceTask/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace InheritanceTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Account user= new Account();
+            user.Username = Console.ReadLine();
+            user.Password = Console.ReadLine();
+            user.Login(user.Username,user.Password);
+
+
+
+             
+        }
+    }
+}
