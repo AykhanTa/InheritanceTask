a9362d1eb95a2aa76c65d0044d03e74b64169adc
AykhanTa <aykhanft@code.edu.az>
2024-03-01 13:10:12 +0400

    feat: adding task2

--- InheritanceTask/Program.cs
+++ InheritanceTask/Program.cs
@@ -4,14 +4,31 @@
     {
         static void Main(string[] args)
         {
-           Account user= new Account();
-            user.Username = Console.ReadLine();
-            user.Password = Console.ReadLine();
-            user.Login(user.Username,user.Password);
+            //Account user= new Account();
+            //user.Username = Console.ReadLine();
+            //user.Password = Console.ReadLine();
+            //user.Login(user.Username,user.Password);
 
-
-
-             
+            SignUp register= new SignUp();
+            Console.WriteLine("Enter name:");
+            string name= Console.ReadLine();
+            register.Name = name;
+            Console.WriteLine("Enter surname:");
+            string surname = Console.ReadLine();
+            register.Surname = surname;
+            Console.WriteLine("Enter phone number:");
+            string phoneNum = Console.ReadLine();
+            register.PhoneNum = phoneNum;
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            register.Password = password;
+            Console.WriteLine("Enter birthday date:");
+            DateTime birthDay= DateTime.Parse(Console.ReadLine());
+            register.Birthday=birthDay;
+            Console.WriteLine("Enter gender: ");
+            string gender = Console.ReadLine();
+            register.Gender = gender;
+            register.ShowInfo();
         }
     }
 }
---
+++ InheritanceTask/SignUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTask
+{
+    internal class SignUp
+    {
+        public string Name;
+        public string Surname;
+        public string PhoneNum;
+        public string Password;
+        public DateTime Birthday;
+        public string Gender;
+
+        public SignUp()
+        {
+            
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name} Surname:{Surname} PhoneNum:{PhoneNum} BirthDay:{Birthday} Gender:{Gender}");
+        }
+       
+        
+    }
+
+}
